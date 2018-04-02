8c809ff6153471399890b6906042996da9a54f2b
Jim Curtis <jim.curtis@redhat.com>
2018-04-02 16:52:27 -0700

    Update pre-scenario-setup.sh

--- 3-greeting/pre-scenario-setup.sh
+++ 3-greeting/pre-scenario-setup.sh
@@ -13,7 +13,28 @@ oc new-project faas --display-name="FaaS- Apache OpenWhisk"
 oc adm policy add-role-to-user admin developer -n faas
 oc process -f https://git.io/openwhisk-template | oc create -f -
 
-./wait-for-ow-ready.sh
+PASSED=false
+TIMEOUT=0
+until $PASSED || [ $TIMEOUT -eq 60 ]; do
+  OC_DEPLOY_STATUS=$(oc get pods -o wide | grep -m 1 "controller" | awk '{print $3}')
+  if [ "$OC_DEPLOY_STATUS" == "Running" ]; then
+    PASSED=true
+    break
+  fi
+  let TIMEOUT=TIMEOUT+1
+  sleep 10
+done
+PASSED=false
+TIMEOUT=0
+until $PASSED || [ $TIMEOUT -eq 5 ]; do
+  INVOKER_HEALTH=$(oc logs controller-0 -n faas | grep "invoker status changed" | grep " Healthy" | awk '{print $11}')
+  if [ "$INVOKER_HEALTH" == "Healthy" ]; then
+    PASSED=true
+    break
+  fi
+  let TIMEOUT=TIMEOUT+1
+  sleep 5
+done
 
 oc patch route openwhisk --namespace faas -p '{"spec":{"tls": {"insecureEdgeTerminationPolicy": "Allow"}}}'
 AUTH_SECRET=$(oc get secret whisk.auth -o yaml | grep "system:" | awk '{print $2}' | base64 --decode)
